93525cfb78080e18e22e3acbaabe02650a124af1
spleereak <123492072+spleereak@users.noreply.github.com>
2024-08-19 13:40:41 +0300

    Merge remote-tracking branch 'origin/main'

--- src/App.js
+++ src/App.js
@@ -10,7 +10,63 @@ function App() {
 
   return (
     <div className="App">
-      <h1>It's work!</h1>
+      <header>
+        <div className='lvl'>
+         <span>Your lvl</span>
+        </div>
+        <div className='nick'>
+          <h2>Your nick</h2>
+        </div>
+        <div className='top'>
+          <span />
+        </div>
+        <div className='settings'>
+          <span />
+        </div>
+      </header>
+      <main>
+        <div className='balance'>
+          <h1>Your balance</h1>
+        </div>
+        <div className='bandit'>
+          <div className='' />
+        </div>
+        <div className='counter'>
+          <div className='counter__skam'>
+            <span>Count of reffs</span>
+          </div>
+          <div className='counter__tickets'>
+            <span>Count of tickets</span>
+          </div>
+        </div>
+      </main>
+      <div className='navbar'>
+        <a href='/scam'>
+          <button>
+            <svg></svg>
+          </button>
+        </a>
+        <a href='/history'>
+          <button>
+            <svg></svg>
+          </button>
+        </a>
+        <a href='/top'>
+          <button>
+            <svg></svg>
+          </button>
+        </a>
+        <a href='/shop'>
+          <button>
+            <svg></svg>
+          </button>
+        </a>
+        <a href='/casino'>
+          <button>
+            <svg></svg>
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
